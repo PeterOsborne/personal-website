cc804f89081d31683214ce731c5841083d7ba316
Peter Osborne <peter@Peters-MBP.lan>
2022-09-29 17:06:08 +1000

    fixing routing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />} />
           <Route path="hello" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
