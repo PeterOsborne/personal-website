94679aa08cbece53400e1101ea70f5ecdb7c7690
Peter Osborne <peter@Peters-MBP.lan>
2022-09-29 17:30:33 +1000

    fixing routing again sorry

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, redirect as Redirect } from 'react-router-dom'
 import Layout from './components/Layout' 
 import Home from './components/Home'
 import About from './components/About'
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="" element={<Home />} />
           <Route path="hello" element={<Home />} />
+          <Route path="PersonalWebsite" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Route>
